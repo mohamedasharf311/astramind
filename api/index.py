38d390b9b7ba0e3b9e34e8bc2ffd40f53b1df70f
MoHAMeD AashRaF <151292203+mohamedasharf311@users.noreply.github.com>
2026-01-17 22:02:27 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,9 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 import sys
-
+# في الأعلى بعد imports
+from fb_webhook import fb
+app.register_blueprint(fb)
 app = Flask(__name__)
 CORS(app)
 
